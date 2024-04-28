53db4721fe8b36921d78c74bc7e64869de1ec604
idermailer <idermailer@gmail.com>
2024-04-28 10:24:49 +0900

    초기 커밋.

---
+++ OutfitDisable/AnimationsReplaceList.cs
@@ -0,0 +1,25 @@
+﻿namespace OutfitDisable
+{
+    public sealed class AnimationsReplaceList
+    {
+        public class AnimationReplaceTarget
+        {
+            public readonly Dictionary<string, string>? referenceAnimations;
+            public readonly Dictionary<string, string>? manualAnimations;
+
+            public AnimationReplaceTarget(Dictionary<string, string>? referenceAnimations, Dictionary<string, string>? manualAnimations)
+            {
+                this.referenceAnimations = referenceAnimations;
+                this.manualAnimations = manualAnimations;
+            }
+        }
+
+        // Relative NPC -> animation to replace and animation to use
+        public Dictionary<string, AnimationReplaceTarget> Targets { get; set; }
+
+        public AnimationsReplaceList()
+        {
+            Targets = new Dictionary<string, AnimationReplaceTarget>();
+        }
+    }
+}
---
+++ OutfitDisable/OutfitOverrideList.cs
@@ -0,0 +1,8 @@
+﻿namespace OutfitDisable
+{
+    public sealed class OutfitOverrideList
+    {
+        public bool Default { get; set; } = false;
+        public Dictionary<string, bool> ExceptionsList { get; set; } = new Dictionary<string, bool>();
+    }
+}
